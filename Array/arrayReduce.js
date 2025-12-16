862da2826503360eaa4e7d3ea48ea0ce618de8d1
Sahadat-Hossen1 <sahadat.hossen1435@gmail.com>
2025-12-16 13:15:06 +0600

    before start beginner task

--- Array/arrayReduce.js
+++ Array/arrayReduce.js
@@ -1,3 +1,25 @@
 // give me some intermidiate and advace and also hard levele tast about reduce about 30 to 40 qs
 
+/* 
+const numbers = [1, 22, 39, 47, 56];
 
+// const sum = numbers.reduce((acc, curr,arr) =>{
+//     const data=acc+curr;
+//     return data
+    
+// },0);
+const sum=numbers.reduce((curr,acc)=>{
+    console.log(`curr:${curr}`);
+    console.log(acc);
+    
+})
+console.log("=====");
+
+//
+const sum2=numbers.reduce((acc,curr)=>{
+    console.log(acc);
+    console.log(`curr:${curr}`);
+    
+})
+console.log(sum); // 15
+*/
